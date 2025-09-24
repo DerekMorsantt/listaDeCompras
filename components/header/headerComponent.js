aa486bf278a90be941cbe337e6c1351c6a5bcc0d
DerekDLV <derekdlv@localhost.localdomain>
2025-09-24 15:45:31 -0600

    Descarga en header

--- components/header/headerComponent.js
+++ components/header/headerComponent.js
@@ -1,3 +1,6 @@
+import { crearSeccionCompras } from "../seccion/seccionComponent.js";
+import { downloadAsImage } from "../../control/descargaImagen.js";
+
 function header() {
     let header = document.createElement('header');
     header.className = "header";
@@ -7,9 +10,30 @@ function header() {
     img.id = "btnDownload";
     img.className = "img";
 
+    img.addEventListener("click", () => {
+        document.querySelector(".encabezado-compras").classList.add("ocultar");
+        document.querySelector(".monto-total").classList.add("ocultar");
+        document.querySelector(".caja-editable").classList.add("ocultar");
+        document.querySelector("header").classList.add("ocultar");
+
+        downloadAsImage();
+
+        document.querySelector(".subtitulo-lista").classList.remove("ocultar");
+        document.querySelector(".contenedor-productos").classList.remove("ocultar");
+
+        document.querySelector(".encabezado-compras").classList.add("mostrar");
+        document.querySelector(".monto-total").classList.add("mostrar");
+        document.querySelector(".caja-editable").classList.add("mostrar");
+        document.querySelector("header").classList.add("mostrar");
+
+        setTimeout(() => {
+            window.location.reload();
+        }, 100);
+    });
+
     header.appendChild(img);
 
     return header;
 }
 
-export { header }
+export { header };
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import { header } from "./components/header/headerComponent.js";
 import { crearSeccionCompras } from "./components/seccion/seccionComponent.js";
-import { downloadAsImage } from "./control/descargaImagen.js";
 
 function main() {
     let section = document.createElement('section');
@@ -15,23 +14,6 @@ function main() {
 
 document.body.appendChild(main());
 
-document.querySelector("#btnDownload").addEventListener("click", () => {
 
-    document.querySelector(".encabezado-compras").classList.add("ocultar");
-    document.querySelector(".monto-total").classList.add("ocultar");
-    document.querySelector(".caja-editable").classList.add("ocultar");
-    document.querySelector("header").classList.add("ocultar");
-
-    downloadAsImage();
-
-    document.querySelector(".subtitulo-lista").classList.remove("ocultar");
-    document.querySelector(".contenedor-productos").classList.remove("ocultar");
-       
-    document.querySelector(".encabezado-compras").classList.add("mostrar");
-    document.querySelector(".monto-total").classList.add("mostrar");
-    document.querySelector(".caja-editable").classList.add("mostrar");
-    document.querySelector("header").classList.add("mostrar");
-
-});
 
 
